a39b34664d3843b51ae781ffd2c9ffbafcf41d77
justacow224 <justacow224@gmail.com>
2025-10-17 15:19:46 +0700

    Add easy option for choosing 512 768 1024

--- ML-KEM/GLOBAL.py
+++ ML-KEM/GLOBAL.py
@@ -4,8 +4,66 @@
 # ML-KEM-768    -  AES-192
 # ML-KEM-1024   -  AES-256
 
-# Currently defined for ML-KEM-512 only
-# ML-KEM-512 constants (AES-128)
+
+
+
+# """
+#     --- ML-KEM-512 constants (AES-128) ---
+
+#     Required RBG strength (bits)      128
+#     Encapsulation key sizes (bytes)   800
+#     Decapsulation key sizes (bytes)   1632
+#     Ciphertext sizes (bytes)          768
+#     Shared secret key sizes (bytes)   32
+
+# """
+# n = 256
+# q = 3329
+
+# k = 2
+# eta1 = 3
+# eta2 = 2
+# du = 10
+# dv = 4
+
+
+
+
+
+
+# """
+#     --- ML-KEM-768 constants (AES-192) ---
+
+#     Required RBG strength (bits)      192
+#     Encapsulation key sizes (bytes)   1184
+#     Decapsulation key sizes (bytes)   2400
+#     Ciphertext sizes (bytes)          1088
+#     Shared secret key sizes (bytes)   32
+
+# """
+# n = 256
+# q = 3329
+
+# k = 3
+# eta1 = 2
+# eta2 = 2
+# du = 10
+# dv = 4
+
+
+
+
+
+"""
+    --- ML-KEM-1024 constants (AES-256) ---
+
+    Required RBG strength (bits)      256
+    Encapsulation key sizes (bytes)   1568
+    Decapsulation key sizes (bytes)   3168
+    Ciphertext sizes (bytes)          1568
+    Shared secret key sizes (bytes)   32
+
+"""
 n = 256
 q = 3329
 
@@ -14,9 +72,3 @@ eta1 = 2
 eta2 = 2
 du = 11
 dv = 5
-
-# Required RBG strength (bits)      128
-# Encapsulation key sizes (bytes)   800
-# Decapsulation key sizes (bytes)   1632
-# Ciphertext sizes (bytes)          768
-# Shared secret key sizes (bytes)   32
